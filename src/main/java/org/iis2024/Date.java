ce586a57e4abd01b77942496b24fcc4e2b90e87e
Damián <admin@damianverde.es>
2024-03-12 09:53:02 +0100

    To German

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -21,7 +21,7 @@ import java.text.SimpleDateFormat;
  * month, and year values cannot be changed.
  */
 public class Date {
-  private final int day;
+  private final int tag;
   private final int month;
   private final int year;
   private bool isLeapYear;
