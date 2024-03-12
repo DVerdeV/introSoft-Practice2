267de662113ff234d70e20cac3a80ad82d46f76d
Damián <admin@damianverde.es>
2024-03-12 09:15:42 +0100

    Add method to Date class

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,10 @@ public class Date {
   private final int month;
   private final int year;
 
+  public void methodAddedByOwner() {
+    System.out.println("This method was added by the owner of the repo ;)");
+  }
+
   /**
    * Retrieves the day component of this date.
    *
